e432a1950c1c31761c1f978f6a978d3af6849025
Parikshit Patil <patil8698parikshit@gmail.com>
2023-05-04 22:48:45 +0530

    lambda function improved

--- 05-functions/06_lambda_function.cpp
+++ 05-functions/06_lambda_function.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -6,6 +7,51 @@ int main() {
     return a + b;
   };
 
-  cout << simple_sum(4,5);
+  auto simple_sum2 = [](int a, int b) -> int {
+    return a + b;
+  };
+
+  cout << "simple_sum(4, 5) = " << simple_sum(4,5) << endl;
+  cout << "simple_sum2(4, 5) = " << simple_sum2(4,5) << endl;
+
+  /* 
+   * Local Capture
+   * - capture all variables in scope
+   * [=] all by value
+   * [&] all by reference
+   * - more selective
+   * [=, &x] x by reference, all others by value
+   * [&, =a, =b] a & b by value, all others by reference
+   */
+  int c = 100;
+  int d = 200;
+
+  // capture by reference
+  auto double_c_ref = [&c]() {
+    c = 2 * c;
+    return c;
+  };
+
+  cout << "1. double_by_ref() = " << double_c_ref() << ", value of c = " << c << endl;
+  cout << "2. double_by_ref() = " << double_c_ref() << ", value of c = " << c << endl;
+
+  // c will be read only
+  auto double_c_value = [c]() {
+    return 2 * c;
+  };
+
+  cout << "1. double_by_value() = " << double_c_value() << ", value of c = " << c << endl;
+  cout << "2. double_by_value() = " << double_c_value() << ", value of c = " << c << endl;
+
+  // capture by move
+  vector<int> vec{1,2,3,4,5};
+  cout << "size of vec (before lambda):" << vec.size() << endl; 
+
+  [vec = move(vec)]() {
+    cout << "size of vec (inside lambda):" << vec.size() << endl; 
+  }();
+
+  cout << "size of vec (after lambda):" << vec.size() << endl; 
+
   return 0;
 }
